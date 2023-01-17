67bd12e8dedee111a598556ecdde1cbe9f12fd5c
strick <wen.ping@rela.me>
2023-01-17 10:57:39 +0800

    doc: 代码格式化

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: strick
  * @LastEditors: strick
  * @Date: 2023-01-12 10:17:17
- * @LastEditTime: 2023-01-16 10:52:56
+ * @LastEditTime: 2023-01-17 10:56:21
  * @Description: 入口，自动初始化
  * @FilePath: /web/shin-monitor/src/index.ts
  */
@@ -15,21 +15,27 @@ import { TypeShinParams } from './typings';
  * 默认属性
  */
 const defaults: TypeShinParams = {
+  src: '//127.0.0.1:3000/ma.gif',       // 采集监控数据的后台接收地址
+  psrc: '//127.0.0.1:3000/pe.gif',      // 采集性能参数的后台接收地址
+  pkey: '',                             // 性能监控的项目key
+  subdir: '',                           // 一个项目下的子目录
+  rate: 5,                              // 随机采样率，用于性能搜集，范围是 1~10，10 表示百分百发送
+  version: '',                          // 版本，便于追查出错源
   record: {
-    isOpen: true,           // 是否开启录像
+    isOpen: true,                       // 是否开启录像
     src: '//cdn.jsdelivr.net/npm/rrweb@latest/dist/rrweb.min.js'   // 录像地址
   },
   error: {
-    isFilterErrorFunc: null,        // 需要过滤的代码错误
-    isFilterPromiseFunc: null,      // 需要过滤的Promise错误
+    isFilterErrorFunc: null,            // 需要过滤的代码错误
+    isFilterPromiseFunc: null,          // 需要过滤的Promise错误
   },
   console: {
-    isOpen: true,              // 默认是开启，在本地调试时，可以将其关闭
+    isOpen: true,               // 默认是开启，在本地调试时，可以将其关闭
     isFilterLogFunc: null,      // 过滤要打印的内容
   },
   crash: {
-    isOpen: true,           // 是否监控页面奔溃，默认开启
-    validateFunc: null,     // 自定义页面白屏的判断条件，返回值包括 {success: true, prompt:'提示'}
+    isOpen: true,               // 是否监控页面奔溃，默认开启
+    validateFunc: null,         // 自定义页面白屏的判断条件，返回值包括 {success: true, prompt:'提示'}
   },
   event: {
     isFilterClickFunc: null,    // 在点击事件中需要过滤的元素
@@ -37,15 +43,9 @@ const defaults: TypeShinParams = {
   ajax: {
     isFilterSendFunc: null      // 在发送监控日志时需要过滤的通信
   },
-  src: '//127.0.0.1:3000/ma.gif',       // 采集监控数据的后台接收地址
-  psrc: '//127.0.0.1:3000/pe.gif',      // 采集性能参数的后台接收地址
-  pkey: '',         // 性能监控的项目key
-  subdir: '',       // 一个项目下的子目录
-  rate: 5,          // 随机采样率，用于性能搜集，范围是 1~10，10 表示百分百发送
-  version: '',      // 版本，便于追查出错源
   identity: {
-    value: '',          // 自定义的身份信息字段
-    getFunc: null,      // 自定义的身份信息获取函数
+    value: '',                  // 自定义的身份信息字段
+    getFunc: null,              // 自定义的身份信息获取函数
   }, 
 };
 // 外部可以调用的属性
